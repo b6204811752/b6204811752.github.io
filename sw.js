1c962e922a3dc6c3f3f88ba08cdc9686abccf3dc
Basant kumar <basantkr762@gmail.com>
2026-02-17 13:09:32 +0530

    Update service worker cache version to v15

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 // Service Worker for Car Rental Ranchi
-// Version 4.0.1 - Network-first for all resources - February 17, 2026
+// Version 4.0.2 - Network-first for all resources - February 17, 2026
 
-const CACHE_NAME = 'car-rental-ranchi-v14';
-const STATIC_CACHE = 'static-v14';
-const DYNAMIC_CACHE = 'dynamic-v14';
-const IMAGE_CACHE = 'images-v14';
+const CACHE_NAME = 'car-rental-ranchi-v15';
+const STATIC_CACHE = 'static-v15';
+const DYNAMIC_CACHE = 'dynamic-v15';
+const IMAGE_CACHE = 'images-v15';
 
 const urlsToCache = [
     '/',
